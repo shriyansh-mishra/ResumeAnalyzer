33de40b56eeab5e797a83ca3f169e97bb3363cb6
Shriyansh Mishra <shriyansh925@gmail.com>
2025-06-18 20:53:30 +0530

    port changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://127.0.0.1:5174'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   credentials: true,
