82643b49c33d864b5e8a69debdb0b5ef4e34fc4a
Shriyansh Mishra <shriyansh925@gmail.com>
2025-08-02 10:23:10 +0530

    increased timeout 30->60 sec

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -13,7 +13,7 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
   withCredentials: true,
-  timeout: 60000, // 30 seconds timeout
+  timeout: 60000, // 60 seconds timeout
 });
 
 
